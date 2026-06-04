efd688ed2a44733aa187f8ff1eddfd1deafb5c55
Sneha Manandhar <snehamanandhar54@gmail.com>
2026-06-04 22:01:37 +0545

    splash screen added

---
+++ localkart/lib/app/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:localkart/core/feature/splash/presentation/pages/splash_screen.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}

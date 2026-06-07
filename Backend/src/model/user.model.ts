2045930210ef379472508f96d5da0a40f772827b
Sneha Manandhar <snehamanandhar54@gmail.com>
2026-06-07 20:10:00 +0545

    add user types and models and repository made

---
+++ Backend/src/model/user.model.ts
@@ -0,0 +1,24 @@
+import mongoose, { Document, Schema } from "mongoose";
+import { UserType } from "../types/user.type";
+const UserSchema: Schema = new Schema<UserType>(
+    {
+        email: { type: String, required: true, unique: true, minlength: 5 },
+        password: {
+            type: String, required: true, minlength: 8,},
+        name: { type: String },
+        role: { type: String, enum: ["Customer","Shopkeeper", "admin"], default: "Customer" },
+        imageUrl: { type: String, required: false },
+        phone: { type: String, required: false },
+        address: { type: String, required: false },
+    },
+    { timestamps: true } 
+);
+
+export interface IUser extends UserType, Document {
+    _id: mongoose.Types.ObjectId;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export const UserModel = mongoose.model<IUser>("User", UserSchema);
+
---
+++ Backend/src/types/user.type.ts
@@ -0,0 +1,13 @@
+import z from "zod";
+
+export const userSchema = z.object({
+    email: z.email().min(5),
+    password: z.string().min(8).optional(),
+    name: z.string().optional(),
+    phone: z.string().optional(),
+    role: z.enum(["Customer","Shopkeeper", "admin"]).default("Customer"),
+    imageUrl: z.string().optional(),
+    address: z.string().optional(),
+});
+
+export type UserType = z.infer<typeof userSchema>;

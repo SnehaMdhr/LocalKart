fc0d79211cb671d48e656c322fd5f5d959e7c1bc
Sneha Manandhar <snehamanandhar54@gmail.com>
2026-06-08 22:08:50 +0545

    added Hive service and model for user authentication management

---
+++ localkart/lib/core/constants/hive_table_constants.dart
@@ -0,0 +1,8 @@
+class HiveTableConstant {
+  HiveTableConstant._();
+
+  static const String dbName = "munch_nearby";
+
+  static const int userTypeId = 0;
+  static const String userTable = "user_table";
+}

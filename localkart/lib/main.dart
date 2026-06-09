c437a5347a5489919427f2c838c4d9e75ff0d413
Sneha Manandhar <snehamanandhar54@gmail.com>
2026-06-09 22:34:07 +0545

    restructure main function to initialize Hive service and shared preferences

--- localkart/lib/main.dart
+++ localkart/lib/main.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:localkart/core/services/hive/hive.service.dart';
+import 'package:localkart/core/services/storage/user_session_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'app/app.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
- 
+  final hiveService = HiveService();
+  await hiveService.init();
+
+  final sharedPreferences = await SharedPreferences.getInstance();
+
   runApp(
-   
-      const App(),
+    ProviderScope(
+      overrides: [
+        sharedPreferencesProvider.overrideWithValue(sharedPreferences),
+      ],
+      child: const App(),
+    ),
   );
 }

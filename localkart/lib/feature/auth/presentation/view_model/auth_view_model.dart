6381dcdbc75398a6a743760c72570e151d93d8b7
Sneha Manandhar <snehamanandhar54@gmail.com>
2026-06-09 22:32:50 +0545

    auth state and view model

---
+++ localkart/lib/feature/auth/presentation/view_model/auth_view_model.dart
@@ -0,0 +1,121 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:localkart/core/services/storage/token_service.dart';
+import 'package:localkart/feature/auth/domain/usecases/get_current_user_usecase.dart';
+import 'package:localkart/feature/auth/domain/usecases/logout_usecase.dart';
+import 'package:localkart/feature/auth/presentation/states/auth_state.dart';
+import '../../domain/usecases/login_usecase.dart';
+import '../../domain/usecases/register_usecase.dart';
+
+final authViewModelProvider = NotifierProvider<AuthViewModel, AuthState>(
+  () => AuthViewModel(),
+);
+
+class AuthViewModel extends Notifier<AuthState> {
+  late final RegisterUsecase _registerUsecase;
+  late final LoginUsecase _loginUsecase;
+  late final GetCurrentUserUsecase _getCurrentUserUsecase;
+  late final LogoutUsecase _logoutUsecase;
+
+  @override
+  AuthState build() {
+    _registerUsecase = ref.read(registerUsecaseProvider);
+    _loginUsecase = ref.read(loginUsecaseProvider);
+    _getCurrentUserUsecase = ref.read(getCurrentUserUsecaseProvider);
+    _logoutUsecase = ref.read(logoutUsecaseProvider);
+    return AuthState();
+  }
+
+  Future<void> register({
+    required String name,
+    required String email,
+    required String phone,
+    // required String role,
+    required String username,
+    required String password,
+    required String confirmPassword,
+  }) async {
+    state = state.copyWith(status: AuthStatus.loading);
+    await Future.delayed(Duration(seconds: 2));
+    final params = RegisterUsecaseParams(
+      name: name,
+      email: email,
+      phone: phone,
+      // role: role,
+      username: username,
+      password: password,
+      confirmPassword: confirmPassword,
+    );
+    final result = await _registerUsecase(params);
+    result.fold(
+      (failure) {
+        state = state.copyWith(
+          status: AuthStatus.error,
+          errorMessage: failure.message,
+        );
+      },
+      (isRegistered) {
+        if (isRegistered) {
+          state = state.copyWith(status: AuthStatus.registered);
+        }
+      },
+    );
+  }
+
+  Future<void> login({required String email, required String password}) async {
+    state = state.copyWith(status: AuthStatus.loading);
+    await Future.delayed(Duration(seconds: 2));
+    final params = LoginUsecaseParams(email: email, password: password);
+    final result = await _loginUsecase(params);
+
+    result.fold(
+      (failure) {
+        state = state.copyWith(
+          status: AuthStatus.error,
+          errorMessage: failure.message,
+        );
+      },
+      (authEntity) {
+        state = state.copyWith(
+          status: AuthStatus.authenticated,
+          authEntity: authEntity,
+        );
+      },
+    );
+  }
+
+  Future<void> logout() async {
+    state = state.copyWith(status: AuthStatus.loading);
+
+    final result = await _logoutUsecase();
+
+    result.fold(
+      (failure) => state = state.copyWith(
+        status: AuthStatus.error,
+        errorMessage: failure.message,
+      ),
+      (success) => state = state.copyWith(
+        status: AuthStatus.unauthenticated,
+        authEntity: null,
+      ),
+    );
+  }
+
+  Future<void> fetchCurrentUser() async {
+    final token = ref.read(tokenServiceProvider).getToken();
+    if (token == null || token.trim().isEmpty) {
+      return;
+    }
+
+    final result = await _getCurrentUserUsecase();
+
+    result.fold(
+      (failure) => state = state.copyWith(errorMessage: failure.message),
+      (authEntity) => state = state.copyWith(
+        status: AuthStatus.authenticated,
+        authEntity: authEntity,
+      ),
+    );
+  }
+
+  
+}
